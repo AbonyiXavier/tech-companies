6bdbb55186a815e2311f5d6057a2aa7be2d3e65b
AbonyiXavier <francisabonyi@gmail.com>
2020-08-22 12:42:27 +0100

    db changes

--- src/database/config.js
+++ src/database/config.js
@@ -2,22 +2,37 @@ import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pg;
+let connectionString = process.env.TEST_DB;
+let ssl = false;
+
+if (process.env.NODE_ENV === "test") {
+  connectionString = process.env.TEST_DB;
+} else {
+  connectionString = process.env.TEST_DB;
+}
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  ssl: false,
-  idleTimeoutMillis: 3600000,
-  connectionTimeoutMillis: 3600000,
+  connectionString,
+  ssl,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
+
+// const pool = new Pool({
+//   host: process.env.PGHOST,
+//   port: process.env.PGPORT,
+//   database: process.env.PGDATABASE,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   ssl: false,
+//   idleTimeoutMillis: 3600000,
+//   connectionTimeoutMillis: 3600000,
+// });
 pool.on("connect", () => {
-  console.log(`Connected to Database ${process.env.PGDATABASE}`);
+  console.log(`Connected to Database on  ${connectionString}`);
 });
 pool.on("error", () => {
-  console.log(`Error occured while connecting to ${process.env.PGDATABASE}`);
+  console.log(`Error occured while connecting to ${connectionString}`);
 });
 
 export default pool;
